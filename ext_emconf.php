245a300a1b1d3cf7c63329e14609b885b4601126
Jaume Presas <jpresas@w-4.com>
2023-03-17 17:07:28 +0100

    [TASK] Updated hooks, version and c¡documentation

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,7 +7,7 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'fe',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.5.99'
+            'typo3' => '11.5.0-12.0.99'
         ],
     ],
     'state' => 'stable',
--- ext_localconf.php
+++ ext_localconf.php
@@ -3,20 +3,22 @@
 defined('TYPO3') or die();
 
 use \TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use \W4Services\W4Cacheautoclear\Hooks\Cacheautoclear;
+use \W4Services\W4Cacheautoclear\Hooks\Filecacheautoclear;
 
-( function( $_EXTKEY) {
+( function() {
 
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][$_EXTKEY . '_auto_cache_clear'] =
-        \W4Services\W4Cacheautoclear\Hooks\Cacheautoclear::class;
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['w4_cacheautoclear_auto_cache_clear'] =
+        Cacheautoclear::class;
 
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_extfilefunc.php']['processData'][$_EXTKEY . '_auto_cache_clear'] =
-        \W4Services\W4Cacheautoclear\Hooks\Filecacheautoclear::class;
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_extfilefunc.php']['processData']['w4_cacheautoclear_auto_cache_clear'] =
+        Filecacheautoclear::class;
 
     /* Load setup */
     ExtensionManagementUtility::addTypoScript(
-        $_EXTKEY,
+        'w4_cacheautoclear',
         'setup',
-        '<INCLUDE_TYPOSCRIPT: source="DIR:EXT:' . $_EXTKEY . '/Configuration/TypoScript/Setup/" extensions="typoscript">'
+        '<INCLUDE_TYPOSCRIPT: source="DIR:EXT:w4_cacheautoclear/Configuration/TypoScript/Setup/" extensions="typoscript">'
     );
 
-})( 'w4_cacheautoclear');
+})();
